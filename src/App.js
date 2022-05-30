32d512c993717bce6dc39c8b4191fbae29f6e722
palakgodhani26 <palakgodhani26@gmail.com>
2022-05-30 15:59:57 +0530

    30-5-5 task

--- src/App.js
+++ src/App.js
@@ -37,47 +37,47 @@ import Demo from './Components/Demo';
 //     }
   
 
-const person = [
-  {
-      name: "Amit",
-      age: 25,
-      course: [
-          "C",
-          "HTML"
-      ]
-  },
-  {
-      name: "Ajay",
-      age: 40,
-      course: [
-          "Java",
-          "JavaScript"
-      ]
-  }
-]
-person.map((p) =>{
-  console.log(person.name, person.age);
-  person.course.map((p) => console.log(p))
-});
+// const person = [
+//   {
+//       name: "Amit",
+//       age: 25,
+//       course: [
+//           "C",
+//           "HTML"
+//       ]
+//   },
+//   {
+//       name: "Ajay",
+//       age: 40,
+//       course: [
+//           "Java",
+//           "JavaScript"
+//       ]
+//   }
+// ]
+// person.map((p) =>{
+//   console.log(person.name, person.age);
+//   person.course.map((p) => console.log(p))
+// });
 
-// const myObj = {
-//   name: "John",
-//   age: 30,
-//   cars: {
-//       car1: "Ford",
-//       car2: "BMW",
-//       car3: "Fiat"
-//   }
-// }
-//   for(let k in myObj) {
-//     if(k === 'cars'){
-//       for(let j in myObj[k]) {
-//         console.log(myObj[k][j]);
-//       }
-//     }else{
-//       console.log(myObj[k]);
-//     }
-//   }
+const myObj = {
+  name: "John",
+  age: 30,
+  cars: {
+      car1: "Ford",
+      car2: "BMW",
+      car3: "Fiat"
+  }
+}
+  for(let k in myObj) {
+    if(k === 'cars'){
+      for(let j in myObj[k]) {
+        console.log(myObj[k][j]);
+      }
+    }else{
+      console.log(myObj[k]);
+    }
+  }
   return (
     <Demo />
   );
