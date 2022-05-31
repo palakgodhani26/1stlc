bd32952b7541dff9224fcd549d62f0c05743abe0
palakgodhani26 <palakgodhani26@gmail.com>
2022-05-31 15:13:32 +0530

    31-5-2 task

--- src/App.js
+++ src/App.js
@@ -5,69 +5,64 @@ import Demo from './Components/Demo';
  function App() {
    let data =[
       {
-        id: 101,
-        name: 'Abacavir',
-        quantity: 25,
-        price: 150,
-        expiry: 2022,
+        name: "amit",
+        age: 35,
+        salary: 40000,
+        bonus: 1000,
         status: true
       },
       {
-        id: 102,
-        name: 'Eltrombopag',
-        quantity: 90,
-        price: 550,
-        expiry: 2021,
-        status: true
-      },
-      {
-        id: 103,
-        name: 'Meloxicam',
-        quantity: 85,
-        price: 450,
-        expiry: 2025,
+        name: "ajay",
+        age: 25,
+        salary: 38000,
+        bonus: 2000,
         status: false
       },
       {
-        id: 104,
-        name: 'Allopurinol',
-        quantity: 50,
-        price: 600,
-        expiry: 2023,
+        name: "mayur",
+        age: 23,
+        salary: 50000,
+        bonus: 500,
         status: true
       },
       {
-        id: 105,
-        name: 'Phenytoin',
-        quantity: 63,
-        price: 250,
-        expiry: 2021,
-        status: false
-      }
-    ];
+        name: "jay",
+        age: 29,
+        salary: 35000,
+        bonus: 600,
+        status: true
+      },
+      {
+        name: "raj",
+        age: 33,
+        salary: 22000,
+        bonus: 2000,
+        status: true
+      },
+    ]
 
 
 return (
   <div>
     <table border="1">
       <tr>
-        <th>Id</th>
         <th>Name</th>
-        <th>Quantity</th>
-        <th>Price</th>
-        <th>Expiry</th>
+        <th>Age</th>
+        <th>Salary</th>
+        <th>Bonus</th>
         <th>Status</th>
       </tr>
       {
         data.map((d,i) => {
-          return(<tr>
-            <td>{d.id}</td>
+          return(
+          <tr>
             <td>{d.name}</td>
-            <td>{d.quantity}</td>
-            <td>{d.price}</td>
-            <td>{d.expiry}</td>
+            <td>{d.age}</td>
+            <td>{d.salary}</td>
+            <td>{d.bonus}</td>
             <td>{d.status.toString()}</td>
-          </tr>)
+          </tr>
+          )
         })
       }
     </table>
