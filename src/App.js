f1ca68a39aba8f65405996a6ab39746aa86f6309
palakgodhani26 <palakgodhani26@gmail.com>
2022-06-06 15:24:05 +0530

    6-6-time task

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import City from './Containers/City';
 import Country from './Containers/Country';
 import CityFun from './Containers/CityFun';
 import CountryFun from './Containers/CountryFun';
+import Time from './Containers/Time/Time';
 
 
 class App extends Component {
   render() {
     return (
       <div>
-        <City />
-        <CityFun />
-        <Country />
-        <CountryFun />
+        {/* <City /> */}
+        {/* <CityFun /> */}
+        {/* <Country /> */}
+        {/* <CountryFun /> */}
+        <Time />
       </div>
     );
   }
